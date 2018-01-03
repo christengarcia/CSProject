3a7aaba09f74e4aa36c9ee8dcf22fc0056adf4ff
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-03 22:21:08 +1100

    Added while year loop

--- CSProject/Program.cs
+++ CSProject/Program.cs
@@ -9,6 +9,21 @@ namespace CSProject
             List<Staff> myStaff = new List<Staff>();
             FileReader fr = new FileReader();
             int month = 0, year = 0;
+
+            while (year == 0)
+            {
+                Console.Write("\nPlease enter the year: ");
+
+                try
+                {
+                    year = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message + "Please try again.");
+                }
+            }
+
         }
     }
 }
