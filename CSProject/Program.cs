8f74d5ef197c223d6976aaa70f9ac8b7ed332bcb
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-03 22:15:55 +1100

    Initial commit

---
+++ CSProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Staff> myStaff = new List<Staff>();
+            FileReader fr = new FileReader();
+            int month = 0, year = 0;
+        }
+    }
+}
