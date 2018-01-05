8217007633d956911f640c8061324255a0a89cca
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-05 23:00:44 +1100

    Added  if statement (result[1] == "Manager")

--- CSProject/Program.cs
+++ CSProject/Program.cs
@@ -132,9 +132,20 @@ namespace CSProject
                     {
                         result = sr.ReadLine().Split(separator, StringSplitOptions.RemoveEmptyEntries);
 
+                        if (result[1] == "Manager")
+                            myStaff.Add(new Manager(result[0]));
+                        else if (result[1] == "Admin")
+                            myStaff.Add(new Admin(result[0]));
                     }
+                    sr.Close();
                 }
+                }else
+                {
+                    Console.WriteLine("Error: File does not exist");
+                }
+            return myStaff;
             }
         }
+
     }
-}
+
