912a541288a474010664ed14b82fdfb204a0e36e
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-06 11:28:29 +1100

    Added method PaySlip (int payMoth, int payYear)

--- CSProject/Program.cs
+++ CSProject/Program.cs
@@ -155,6 +155,13 @@ namespace CSProject
 
         enum MonthsOfYear { JAN = 1, FEB = 2, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC, }
 
+        public PaySlip(int payMonth, int payYear)
+        {
+            month = payMonth;
+            year = payYear;
+        }
+
+
     }
 }
 
